1ec8e8046dc20696a7ad0ec3af8454398ab24d0c
Raquel Leão <vc.raquel@gmail.com>
2021-11-19 11:31:14 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/Lead_Colector/LeadColectorApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Lead_Colector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadColectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadColectorApplication.class, args);
+	}
+
+}
