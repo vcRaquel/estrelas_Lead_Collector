ad7496f62faaa94fb839f59f47217b926f7a90e6
Raquel Leão <vc.raquel@gmail.com>
2021-11-20 12:34:29 -0300

    criar interface de comunicacao com o banco de dados para Produto

---
+++ src/main/java/br/com/zup/Lead_Colector/produtos/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.com.zup.Lead_Colector.produtos;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+
+}
