6203d38eb95b3fff6b663fd79ecc25c823559fa6
Raquel Leão <vc.raquel@gmail.com>
2021-11-20 14:31:23 -0300

    criar query de busca de Lead pelo nome do Produto

--- src/main/java/br/com/zup/Lead_Colector/Leads/LeadRepository.java
+++ src/main/java/br/com/zup/Lead_Colector/Leads/LeadRepository.java
@@ -2,6 +2,10 @@ package br.com.zup.Lead_Colector.Leads;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface LeadRepository extends CrudRepository<Lead, String> {
+import java.util.List;
 
+public interface LeadRepository extends CrudRepository<Lead, String> {
+    //procurar no banco de dados todos os leads que tenham o
+    // produto com o nome do produto na sua lista de produtosDeInteresse
+    List<Lead>findAllByProdutosDeInteresseNome(String nomeDoProduto);
 }
