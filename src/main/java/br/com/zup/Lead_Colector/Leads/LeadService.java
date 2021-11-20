c735f2869a1cdc6eea0fa55bbcd0d82781894677
Raquel Leão <vc.raquel@gmail.com>
2021-11-20 14:36:05 -0300

    criar servico para Lead

---
+++ src/main/java/br/com/zup/Lead_Colector/Leads/LeadService.java
@@ -0,0 +1,11 @@
+package br.com.zup.Lead_Colector.Leads;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeadService {
+    @Autowired
+    LeadRepository leadRepository;
+
+}
