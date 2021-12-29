9eda26a766b3449cc723fde9b8ed4b67d6fa0d4a
Raquel Leão <vc.raquel@gmail.com>
2021-12-29 17:03:53 -0300

    criar classe de MensagemDeErro para ser usada nas manipulacoes de Exceptions

--- src/main/java/br/com/zup/Lead_Colector/config/MensagemDeErro.java
+++ src/main/java/br/com/zup/Lead_Colector/config/MensagemDeErro.java
@@ -1,2 +1,17 @@
-package br.com.zup.Lead_Colector.config;public class MensagemDeErro {
+package br.com.zup.Lead_Colector.config;
+
+public class MensagemDeErro {
+    private String mensagem;
+
+    public MensagemDeErro(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
 }
