e16741ec4851576ac24eeb83d97ef48e6f44509c
Raquel Leão <vc.raquel@gmail.com>
2021-12-29 17:05:16 -0300

    criar classe ControllerAdvice e manipular MethodArgumentNotValidException

--- src/main/java/br/com/zup/Lead_Colector/config/ControllerAdvice.java
+++ src/main/java/br/com/zup/Lead_Colector/config/ControllerAdvice.java
@@ -1,2 +1,18 @@
-package br.com.zup.Lead_Colector.config;public class ControllerAdvice {
+package br.com.zup.Lead_Colector.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MensagemDeErro manipularMethodArgumentNotValidException (MethodArgumentNotValidException exception){
+    return new MensagemDeErro(exception.getMessage());
+    }
+
 }
